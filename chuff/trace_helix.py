a4b0b633d3c3773e855d02a81b5d1071979ef8af
Xueqi Liu <xueqi.liu@gmail.com>
2015-06-15 10:56:59 -0400

    modify trace helix

--- chuff/trace_helix.py
+++ chuff/trace_helix.py
@@ -2,6 +2,8 @@ import math
 import os
 from matplotlib import pyplot as plt
 
+R_TO_D = 180 / math.pi
+
 def trace_helix(aligns, ori_psis, ori_xys, helical_twist = 167.1, helical_rise = 27.44, apix = 1.08714):
 
     '''
@@ -28,6 +30,10 @@ def trace_helix(aligns, ori_psis, ori_xys, helical_twist = 167.1, helical_rise =
     for i in range(len(ori_xys)):
         new_xys.append([ori_xys[i][0] + dxs[i], ori_xys[i][1] + dys[i]])
 
+    newx,newy = zip(*new_xys)
+    from matplotlib import pyplot as plt
+    plt.plot(newx, newy, 'x-')
+    plt.show()
     dds = [0]
     for i in range(1, len(new_xys)):
         x1, y1 = new_xys[i]
@@ -36,6 +42,74 @@ def trace_helix(aligns, ori_psis, ori_xys, helical_twist = 167.1, helical_rise =
         dds.append(math.sqrt(d2))
     dxs, dys = zip(*[undo_shift(dxs[i], dys[i], ori_psis[i]) for i in range(len(dxs))])
     fit(dds, angs, phi_diffs, helical_twist, helical_rise / apix)
+
+
+def smooth_xy(xys):
+
+    from scipy.interpolate import UnivariateSpline
+
+    xs, ys = zip(*xys)
+
+    spl = UnivariateSpline(xs, ys)
+    ys1 = spl(xs)
+
+    spl = UnivariateSpline(ys1, xs)
+    xs1 = spl(ys1)
+    return zip(xs1, ys1)
+
+def get_dist(xy1, xy2):
+    '''
+        get distance between two points
+    '''
+    dx = xy1[0] - xy2[0]
+    dy = xy1[1] - xy2[1]
+    return math.sqrt(dx * dx + dy * dy)
+
+def even_xys(xys, angles = None):
+    '''
+        even xys takes an evened xys coordinate to even the distance between xys,
+        and return the new xys
+    '''
+    dists = [0]
+    for i in range(2, len(xys)):
+        dists.append(get_dist(xys[i], xys[i - 1]))
+        if angles is not None:
+            ang = angles[i] * R_TO_D
+            dists[i] = dists[i] / math.sin(2 * ang) / 2 * ang
+
+
+
+def curvature_angles(thetas, psis):
+    cs = [0. for _ in thetas]
+    for i in range(1, len(thetas)):
+        theta1, theta2 = thetas[i - 1: i + 1]
+        psi1, psi2 = thetas[i - 1: i + 1]
+        cs[i] = get_curve_angle(theta1, psi1, theta2, psi2)
+    return cs
+
+
+def get_curve_angle(theta1, psi1, theta2, psi2):
+    from math import tan, acos, sqrt, pi
+    pi1 = pi / 180
+    theta1, psi1, theta2, psi2 = [ang * pi1 for ang in [theta1, psi1, theta2, psi2]]
+    a1 = (tan(psi1), tan(theta1), 1)
+    a2 = (tan(psi2), tan(theta2), 1)
+    a12 = sum([aa * aa for aa in a1])
+    a22 = sum([aa * aa for aa in a2])
+    ca = (a1[0] * a2[0] + a1[1] * a2[1] + a1[2] * a2[2]) / sqrt(a12 * a22)
+    ang = acos(ca) / pi1
+    return ang
+
+def smooth_angle(ang):
+    '''
+        smooth angles, so that the angle would be continuous change.
+        This is not affected by gap between the data.
+    '''
+
+    from scipy.interpolate import UnivariateSpline
+
+    spl = UnivariateSpline(range(len(ang)), ang)
+    return spl(range(len(ang)))
 
 def window_smooth(arr, window_size):
     import numpy as np
@@ -86,7 +160,7 @@ def fit_integer(dds, dp):
         A0 = ( B - c) / slope
         A0 = np.rint(A0)
         diff = A0 - oldA0
-        if len(np.unique(diff)) == 1: 
+        if len(np.unique(diff)) == 1:
             break
         oldA0 = A0
 #        plt.plot(A0, dds,'o-')
@@ -108,7 +182,8 @@ def fit_integer(dds, dp):
     plt.plot(A0, B - A0 * slope - c, 'o')
     plt.savefig('test.png')
 
-    os.system('convert test.png test.jpg')
+    import Image
+    Image.open('test.png').save('test.jpg')
     return A0, slope, c
 
 def undo_shift(dx, dy, psi):
@@ -146,22 +221,33 @@ def get_curvature(phi1, theta1, psi1, phi2, theta2, psi2, dp):
 if __name__ == "__main__":
     import numpy as np
     import random
-    dp = 20
-    x = [1, 2, 3, 4, 5, 5, 6, 6, 8, 8, 5, 3, 9, 10, 20]
-    dds1 = np.array([x1 * dp + random.randint(-8, 15) * 1.0 for x1 in x])
-    A0 = np.arange(len(dds1))
-    fit_integer(dds1, 20)
-
     #def trace_helix(aligns, ori_psis, ori_xys, helical_twist = 167.1, helical_rise = 27.44):
 
     import sys
-
-    align_param = sys.argv[1]
-    box_file = sys.argv[2]
+    if len(sys.argv) > 2:
+        align_param = sys.argv[1]
+        box_file = sys.argv[2]
+    else:
+        align_param = "sxihrsr_bin4_gold2/output1_graphs/%04d_MT1_1.txt" % int(sys.argv[1])
+        box_file = "scans/%04d_MT1_1_doc.spi" % int(sys.argv[1])
     aligns = open(align_param).read().strip().split("\n")
     aligns = [[float(m) for m in align.split()[:5]] for align in aligns]
     boxes = open(box_file).read().strip().split("\n")
     boxes = [[float(m) for m in box.split()[2:6]] for box in boxes if box.strip() and not box.strip().startswith(";")]
     ori_xys = [box[:2] for box in boxes]
+    smooth_xy(ori_xys, even_dist=1)
+    exit()
+    xs,ys = zip(*ori_xys)
+    from matplotlib import pyplot as plt
+    #plt.plot(xs, ys, 'x')
+
+    A = np.vstack([xs, np.ones(len(xs))]).T
+    B = np.array(ys)
+    r = np.linalg.lstsq(A, B)
+    slope, c = r[0]
+    print r[1], r[2], r[3]
+    print slope, c
+    #plt.plot([xs[0], xs[-1]], [ xs[0] * slope + c, xs[-1] * slope + c])
+    #plt.show()
     ori_psis = [box[3]  for box in boxes]
     trace_helix(aligns, ori_psis, ori_xys)
